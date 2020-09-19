65aa8c5e5263d5bc8eb92ca6e41258fc0b8972bd
r4huln1 <60283873+r4huln1@users.noreply.github.com>
2020-09-18 21:31:36 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="jquery.min.js"></script>
     <script language="JavaScript">
       $(document).ready(function(){
           $('#years').on('change', function(){
