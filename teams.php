c1cf02c339fb6c69c3a422f87a93ecd07db55fef
r4huln1 <60283873+r4huln1@users.noreply.github.com>
2020-11-03 12:15:32 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -3,7 +3,7 @@
         $obj1 = file_get_contents("http://ergast.com/api/f1/" . $_POST['yearsID'] . "/constructors.json");
         $teams = json_decode($obj1);
         $allTeams = Array($teams->MRData->ConstructorTable->Constructors[0]->name=>$teams->MRData->ConstructorTable->Constructors[0]->constructorId);
-        for($x = 0; $x < 13; $x++){
+        for($x = 0; $x < 23; $x++){
             if($teams->MRData->ConstructorTable->Constructors[$x]->name == NULL){
                 continue;
             }
