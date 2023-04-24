55422498ed6dd42c2ef0b1250cecf7609f82160e
rahulnryn <60283873+rahulnryn@users.noreply.github.com>
2023-04-23 23:30:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -112,7 +112,7 @@
               <option value="2020">2020</option>
               <option value="2021">2021</option>
               <option value="2022">2022</option>
-
+              <option value="2023">2023</option>
             </select>
           <br><br>
           <label class="car">Choose a Team:</label>
